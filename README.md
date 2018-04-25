1f4a7c1b7f64c16a57481e8e457c408252b8a16b
Moritz Marquardt <git@mo-mar.de>
2018-04-25 22:25:20 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,22 @@
+# Docker Hosts
+Update a `/etc/hosts`-style file to add docker containers to resolve locally on the host. The application will run until an error occurs and will watch for events from the docker daemon, keeping the file up to date.
+
+**Usage:** `docker-hosts [file=/etc/hosts] [tld=docker]`
+
+Each docker container will be added as `<container-name>.docker`, `<container-short-id>.docker` and `<container-long-id>.docker`.  
+Which network will be used for containers with multiple networks is undefined.
+
+**Tip:** if you want to use this on a dedicated DNS server instead of using the hosts file, I suggest using [CoreDNS](https://coredns.io/) with the [hosts plugin](https://coredns.io/plugins/hosts/).
+
+## Build and installation
+
+If [Go](https://golang.org/) and [run](https://github.com/moqmar/run) are installed, Docker Hosts can be installed simply by running `go get github.com/moqmar/docker-hosts && run -c $GOPATH/src/github.com/moqmar/docker-hosts install`
+
+If you want to install the binary version, use the following instead:
+```
+wget -qO- https://github.com/moqmar/docker-hosts/releases/latest | grep -Eo '/moqmar/docker-hosts/releases/download/[^/]+/docker-hosts' | sudo wget --base=http://github.com/ -i - -O /usr/local/bin/docker-hosts
+sudo chmod +x /usr/local/bin/docker-hosts
+sudo wget -O /etc/systemd/system/docker-hosts.service https://raw.githubusercontent.com/moqmar/docker-hosts/master/docker-hosts.service
+sudo systemctl enable docker-hosts.service
+sudo systemctl start docker-hosts.service
+```
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+var working = false
+var scheduleUpdate = false
+
+// ContainerToHosts converts a Docker Container to a hosts-file ready string.
+func ContainerToHosts(container types.Container, tld string) string {
+	if len(container.NetworkSettings.Networks) < 1 {
+		fmt.Printf("Warning: Container %s is not connected to any network.\n", container.ID)
+		return ""
+	}
+
+	hosts := ""
+
+	for _, details := range container.NetworkSettings.Networks {
+		for _, name := range container.Names {
+			// <name>.docker
+			hosts += details.IPAddress + "\t" + strings.TrimPrefix(name, "/") + "." + tld + "\n"
+		}
+		// <id>.docker
+		hosts += details.IPAddress + "\t" + container.ID[:10] + "." + tld + "\n"
+		hosts += details.IPAddress + "\t" + container.ID + "." + tld + "\n"
+		hosts += "\n"
+		break // Only the first network
+	}
+
+	return hosts
+}
+
+// Update lists all Docker Containers and adds them to the hosts file.
+func Update(docker client.APIClient, file string, tld string, wait bool) {
+	working = true
+	scheduleUpdate = false
+
+	// Wait some time, especially for the "die" event (and currently only used for that) as the container won't get removed immediately from the list.
+	if wait {
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	containers, err := docker.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	hosts := ""
+
+	// Prepare the hosts file
+	dat, err := ioutil.ReadFile(file)
+	if err == nil {
+		hosts += strings.SplitN(string(dat), "### Docker Hosts ###\n\n", 2)[0]
+	}
+	if hosts != "" {
+		hosts = strings.TrimRight(hosts, "\n") + "\n\n"
+	}
+	hosts += "### Docker Hosts ###\n\n"
+
+	// Add containers
+	count := 0
+	for _, container := range containers {
+		hosts += ContainerToHosts(container, tld)
+		count++
+	}
+
+	// Write the hosts file
+	err = ioutil.WriteFile(file, []byte(hosts), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Updated %s, %d active containers.\n", file, count)
+
+	working = false
+	if scheduleUpdate {
+		scheduleUpdate = false
+		Update(docker, file, tld, false)
+	}
+}
+
+// Watch calls Update() automatically every time a container changes
+func Watch(docker client.APIClient, file string, tld string) {
+	go Update(docker, file, tld, false)
+
+	msgChan, errChan := docker.Events(context.Background(), types.EventsOptions{})
+
+	go func() {
+		err := <-errChan
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	for {
+		msg := <-msgChan
+
+		if msg.Type == "container" && (msg.Action == "die" || msg.Action == "destroy" || msg.Action == "stop" || msg.Action == "start") {
+			actor := msg.Actor.ID
+			if name, ok := msg.Actor.Attributes["name"]; ok {
+				actor = name
+			}
+			fmt.Printf("Got event from container %s: %s\n", actor, msg.Action)
+			if working {
+				scheduleUpdate = true
+			} else {
+				go Update(docker, file, tld, msg.Action == "die")
+			}
+		}
+	}
+}
+
+func main() {
+	docker, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	file := "/etc/hosts"
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	tld := "docker"
+	if len(os.Args) > 2 {
+		tld = os.Args[2]
+	}
+
+	Watch(docker, file, tld)
+}
